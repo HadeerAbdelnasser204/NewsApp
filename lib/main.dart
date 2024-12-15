59f79a0a34a21dedfd07049cb1fabda1cd0a357c
Hadeer Abdelnasser <101938448+HadeerAbdelnasser204@users.noreply.github.com>
2024-12-15 15:17:17 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root  of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
